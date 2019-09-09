b8fabaaef6ff134f176c5e70992752c1b3412f91
Placeholder Pigeon <xaxoqual@gmail.com>
2019-09-09 18:52:31 -0400

    Updated scripts, many fixes, cleaned up names, column labels, etc

--- cleanscript.R
+++ cleanscript.R
@@ -1,7 +1,6 @@
 library(dplyr)
 library(tidyr)
 library(readr)
-setwd("c:/Users/Placeholder/Documents/Coursera2/CleanProj")
 
 #read all sources into tables
 tst <- read.table("test/subject_test.txt", header = FALSE)
@@ -10,6 +9,7 @@ ytst <- read.table("test/y_test.txt", header = FALSE)
 trn <- read.table("train/subject_train.txt", header = FALSE)
 xtrn <- read.table("train/X_train.txt", header = FALSE)
 ytrn <- read.table("train/y_train.txt", header = FALSE)
+featlist <- read.table("features.txt", header = FALSE)
 
 #create index variables for later merging and assign to the subject ID frame
 #The test index continues sequentially from the end of the train index
@@ -18,39 +18,57 @@ tstind <- c((nrow(trn)+1):(nrow(trn)+nrow(tst)))
 trn2 <- cbind (trnind, trn)
 tst2 <- cbind(tstind, tst)
 
-#Assign flags for whether a observation is test/train for tracking
-tstb <- "TEST"
-trnb <- "TRAIN"
+#Merge the subject and the activity list
+mergedtst <- cbind(tst2, ytst)
+mergedtrn <- cbind(trn2, ytrn)
 
-#Since we only need the mean + standard deviation for the clean data set
-#We can just extract those and rbind it to 
-trnmeans <- rowMeans(xtrn)
-trnsd <- apply(xtrn, 1, sd)
-tstmeans <- rowMeans(xtst)
-tstsd <- apply(xtst, 1, sd)
-
-#Add Mean and Std
-mergedtst <- cbind(tst2, ytst, tstmeans, tstsd, tstb)
-mergedtrn <- cbind(trn2, ytrn, trnmeans, trnsd, trnb)
+#Index for all of the std & mean colums in the x set
+xcolindex <- c(1:6,41:46,81:86,121:126,161:166,201:202,214:215,
+               227:228,240:241,253:254,266:271,345:350,424:429,
+               503:504,516:517,529:530,542:543)
+#make a vector of the features names with lowercase
+#remove parantheses and hyphens from features names
+featvec <- tolower(as.vector(featlist$V2))
+featvec <- gsub("\\()", "", featvec) 
+featvec <- gsub("-", "_", featvec) 
+featvec <- gsub("^t", "timedomain_", featvec)
+featvec <- gsub("^f", "frequencydomain_",featvec)
+featvec <- gsub("bodybody", "body", featvec)
+#Make a for loop with all feature columns in the index 
+#to bind selected columns into the other data frame
+for (i in xcolindex){
+  colnames(xtst)[i] <- featvec[i]
+  colnames(xtrn)[i] <- featvec[i]
+  mergedtst <- cbind(mergedtst, xtst[i])
+  mergedtrn <- cbind(mergedtrn, xtrn[i])
+}
 
 #Clean column names for binding both sets
-colnames(mergedtst) <- c("Index", "SubjectID", "Activity", "MeasurementMean", "MeasurementStandardDeviation", "SetOrigin")
-colnames(mergedtrn) <- c("Index", "SubjectID", "Activity", "MeasurementMean", "MeasurementStandardDeviation", "SetOrigin")
+colnames(mergedtst)[1:3] <- c("index", "subject_id", "activity")
+colnames(mergedtrn)[1:3] <- c("index", "subject_id", "activity")
 
 #Combine the Test and the Train sets
 mergedfull <- rbind(mergedtrn, mergedtst)
 
 #Replace activity values with appropriate activities
-mergedfull$Activity <- factor(mergedfull$Activity, 
-      levels = c(1, 2, 3, 4, 5, 6),
-      labels = c("WALKING", "WALKING UPSTAIRS", "WALKING DOWNSTAIRS",
-                 "SITTING", "STANDING", "LAYING")
-      )
+mergedfull$activity <- factor(mergedfull$activity, 
+                              levels = c(1, 2, 3, 4, 5, 6),
+                              labels = c("WALKING", "WALKING UPSTAIRS", "WALKING DOWNSTAIRS",
+                                         "SITTING", "STANDING", "LAYING")
+)
 
+#Create Tidy Data file with averages for each data point
+#Grouped by subject and activity type
 tidysum <- mergedfull %>%
-  group_by(SubjectID, Activity) %>%
-  summarize(AverageMean = mean(MeasurementMean), 
-            AverageStdDev = mean(MeasurementStandardDeviation))
+  group_by(subject_id, activity) %>%
+  summarise_at(vars(timedomain_bodyacc_mean_x:frequencydomain_bodygyrojerkmag_std), mean, na.rm = TRUE)
 
-  
+#append average to the column names to designate the data transformation
+for (i in 3:ncol(tidysum)){
+  colnames(tidysum)[i] <- paste("average_", colnames(tidysum)[i], sep="")
+}
+
+#write the table
+write.table(tidysum, file="PBassignment5tidytable.txt", row.name=FALSE)
+
 
